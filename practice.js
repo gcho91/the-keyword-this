502fd91c0600b8d3b9a6233a2e673f1147306269
gcho91 <gcho91@gmail.com>
2017-08-29 14:50:40 -0500

    Made changes to make SpecRunner work. Should be reviewed this weekend

--- practice.js
+++ practice.js
@@ -2,6 +2,7 @@
   // 1) What is the purpose of the 'this keyword'?
 
       //Answer
+      //A pointer to objects, defines context
 
   // 2) What are the four rules that govern what the 'this keyword' is bound to and describe each?
 
@@ -10,6 +11,7 @@
   // 3) What does .bind do?
 
       //Answer
+      //bind() creates a new function that, when called, has its this keyword set to the provided value, with a given sequence of arguments preceding any provided when new function is called.
 
 
 //Next Problem
@@ -20,9 +22,30 @@
   //getUsername --> which is a function that returns the current object's username property. *Don't use 'user' instead use the 'this' keyword*
 
     //Code Here
+      // var user = {
+      //   username: "gcho91",
+      //   email:"gcho91@gmail.com",
+      //   getUsername: function(){
+      //     return this.username;
+      //   }
+      // }
+      // user.getUsername();
+
+
+
+
+    var user = {
+      username: "devmtn101",
+      email: "dev@mtn.com",
+      getUsername: function() {
+        //returns current obj's username prop, use this keyword
+        return this.username;
+      }
+    }
+
 
 //Now, invoke the getUsername method and verify you got the username of the object and not anything else.
-
+// user.getUsername();
 
 //Next Problem
 
@@ -30,6 +53,16 @@
 // Write a constructor function, including method definitions, which will make the following function invocations function properly.
 
   //Function Invocations Here
+  function Car(make, model, year) {
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.move = 0;
+    this.moveCar = function () {
+      this.move+=10;
+      return this.move;
+    }
+  }
 
 var prius = new Car('Toyota', 'Prius', 2011);
 var mustang = new Car('Ford', 'Mustang', 2013);
@@ -54,7 +87,8 @@ var getYear = function(){
 
 //Note(no tests)
   //Code Here
-
+  //use bind to bind the prius object to the function and save it in a var called getPriusYear
+var getPriusYear = getYear.bind(prius);
 
 
 //New Problem
@@ -69,16 +103,17 @@ var getMyUsername = function() {
  return this.username;
 };
 
-var userName = getMyUsername(); //Fix this
+var userName = getMyUsername.call(myUser); //Fix this
+userName();
+
 
 //Above you're given an object, and  a function. What will the getMyUsername function return?
 //Note(no tests)
-  //Answer Here
+  //Undefined, this doesnt refer to anything, there is no this.
 
 //In the example above, what is the 'this keyword' bound to when getMyUsername runs?
-
   //Answer Here
+  //Bound to whatever object is being passed to the function. will bind to myUser
 
 
 //Fix the getMyUsername invocation (stored in the userName variable, at the bottom of the above code) so that userName will be equal to 'iliketurtles'.
-
